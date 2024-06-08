c87cfa7821a842d56054f9686886355c81599b25
Vishnuvallabh <52846629+Vishnuvallabh@users.noreply.github.com>
2024-06-08 16:33:51 +0530

    Integrated with Swiggy API through CORS
    Signed-off-by: Vishnuvallabh <52846629+Vishnuvallabh@users.noreply.github.com>

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
   
 const Body = () => {
 
-  const [updatedResList , setUpdatedResList] = useState(resList);
+  const [listOfRestaurants , setlistOfRestaurants] = useState([]);
 
   const handleTopRatedRestaurants = () => {
-    const filtArr = updatedResList.filter(res => res?.info.avgRating > 4);
-    setUpdatedResList(filtArr);
+    const filtArr = listOfRestaurants.filter(res => res?.info.avgRating > 4);
+    setlistOfRestaurants(filtArr);
     console.log(filtArr)
+  };
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    console.log(data);
+    const json = await data.json();
+    console.log(json.data);
+     setlistOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
   };
 
     return (
@@ -21,7 +33,7 @@ const Body = () => {
             <div className='search'>Search Bar</div>
             <div className='res-container'>
                 {
-                  updatedResList.map((restaurant)=> (
+                  listOfRestaurants.map((restaurant)=> (
                     <RestaurantCard key = {restaurant?.info?.id} resData = {restaurant} />
                   ))
                 }
